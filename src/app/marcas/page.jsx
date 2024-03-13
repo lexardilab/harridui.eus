71cd174447273d5beab17d2ee948915050a732e7
lexardi <lexardi.lab@gmail.com>
2024-03-13 20:02:05 +0100

    logos Marcas

--- src/app/marcas/page.jsx
+++ src/app/marcas/page.jsx
@@ -1,3 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function page() {
-  return <div>marcas</div>;
+  return (
+    <>
+      <div className="grid grid-cols-4 gap-4 py-4 ">
+        <div className="flex items-center justify-center">
+          <Link href="/">
+            <Image
+              src="/StanleyStella_Logo.png"
+              width="200"
+              height="84"
+              alt=""
+            />
+          </Link>
+        </div>
+        <div className="flex items-center justify-center">
+          <Link href="/">
+            <Image
+              src="/StanleyStella_Logo.png"
+              width="200"
+              height="84"
+              alt=""
+            />
+          </Link>
+        </div>
+        <div className="flex items-center justify-center">
+          <Link href="/">
+            <Image
+              src="/StanleyStella_Logo.png"
+              width="200"
+              height="84"
+              alt=""
+            />
+          </Link>
+        </div>
+        <div className="flex items-center justify-center">
+          <Link href="/">
+            <Image
+              src="/StanleyStella_Logo.png"
+              width="200"
+              height="84"
+              alt=""
+            />
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
