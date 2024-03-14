82708c96f1499ea1253631bdf9e2b559a9149018
lexardi <lexardi.lab@gmail.com>
2024-03-14 11:08:41 +0100

    index

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,10 @@ export default function Home() {
   return (
     <div class="flex items-center justify-center h-screen bg-[#eae9b9]">
       <div class="">
-        <h1 class="text-6xl text-center">Harridui</h1>
-        <p class="text-center text-xl">Taller de Serigrafía y Diseño</p>
+        <h1 class="text-6xl text-center font-black">Harridui</h1>
+        <p class="text-center text-xl font-thin">
+          Estudio de Serigrafía y Diseño
+        </p>
       </div>
     </div>
   );
