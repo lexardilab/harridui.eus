671d15d514128e8630e6b05d6b0b0e14b3982132
lexardi <lexardi.lab@gmail.com>
2024-03-14 11:09:37 +0100

    layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,12 @@
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({
+  weight: ["400", "700", "100"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   title: "Harridui",
@@ -12,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className={inter.className}>
+      <body className={montserrat.className}>
         <Navbar />
         {children}
       </body>
