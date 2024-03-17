6f4f9be43677a5a877507a0de44001951b22fd31
lexardi <lexardi.lab@gmail.com>
2024-03-18 00:42:27 +0100

    change index

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="w-full bg-[#eae9b9] items-center justify-center flex">
+    <div className="w-full bg-[#eae9b9] items-center justify-left flex">
       <Link href="/">
         <Image src="/icon_logo.png" width="100" height="100" alt="" />
       </Link>
