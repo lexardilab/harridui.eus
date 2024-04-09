ac886f6475b73c203b7c47534368b314114e3ca4
lexardi <lexardi.lab@gmail.com>
2024-04-09 17:27:37 +0200

    commit

--- src/app/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="bg-[#eae9b9] justify-center flex py-2">
-      <h1 className="font-light text-l">
-        Web construida por Harridui utilizando Next.js + Sanity.io
-      </h1>
-    </div>
-  );
-}
