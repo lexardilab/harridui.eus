3b8ef5a3a6dff9d485c819d09c4419d8dea622f1
lexardi <lexardi.lab@gmail.com>
2025-07-11 09:10:31 +0200

    metadata

--- src/app/marcas/layout.js
+++ src/app/marcas/layout.js
@@ -2,7 +2,7 @@ import "../globals.css";
 import Navbar from "../components/Navbar";
 export const metadata = {
   title: "Harridui | Marcas",
-  description: "Harridui | Taller de serigrafía",
+  description: "Harridui | Trabajamos con las principales marcas del sector, Stanley Stella, Native Spirit, Gildan y muchas más.",
 };
 
 export default function RootLayout({ children }) {
--- src/app/studio/[[...tool]]/layout.js
+++ src/app/studio/[[...tool]]/layout.js
@@ -1,14 +1,13 @@
 export const metadata = {
   title: "Harridui | Marcas",
-  description: "Harridui | Taller de serigrafía",
+  description:
+    "Harridui | Ofrecemos servicios de imagen de marca, asesoría y personalización textil con materiales sostenibles de la mejor calidad.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
